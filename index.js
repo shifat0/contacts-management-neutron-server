22541743d73eae580c9725f51c9fd6d36843c05b
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2024-01-07 10:47:10 +0600

    mongoose connection timeout reduced

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ import contactRouter from "./routes/contacts.js";
 const app = express();
 dotenv.config();
 
-// const connect = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URL);
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
 //Middlewears
 app.use(express.json());
 app.use(cors());
@@ -22,20 +14,12 @@ const api = process.env.API;
 
 app.use(`${api}/contacts`, contactRouter);
 
-// mongoose.connection.on("disconnected", () =>
-//   console.log("mongoDB disconnected")
-// );
-// mongoose.connection.on("connected", () => console.log("mongoDB connected"));
-
-// app.listen(process.env.PORT, () => {
-//   connect();
-//   console.log(`Listening on Port ${process.env.PORT}`);
-// });
-
 // Database Connection
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Database Connected"))
   .catch((err) => console.log(err.message));
 
-app.listen(process.env.PORT, () => console.log(api));
+app.listen(process.env.PORT, () =>
+  console.log(`listening on port ${process.env.PORT}`)
+);
